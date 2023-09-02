739dfccb22007ca0c56ed1b866a8020a69562f44
Null <auguselo77@gmail.com>
2023-09-02 18:31:11 -0300

    return

--- FileManager.go
+++ FileManager.go
@@ -17,34 +17,45 @@ func Copy(sourcePath, destinationPath string) error {
 		if err := os.MkdirAll(destinationPath, os.ModePerm); err != nil {
 			return err
 		}
+	
+		// Lee el contenido de la carpeta de origen
 		entries, err := os.ReadDir(sourcePath)
 		if err != nil {
 			return err
 		}
+	
+		// Recorre los archivos y subdirectorios en la carpeta de origen
 		for _, entry := range entries {
 			sourceFile := filepath.Join(sourcePath, entry.Name())
 			destinationFile := filepath.Join(destinationPath, entry.Name())
-			Copy(sourceFile, destinationFile)
-		}
 	
-    } else {
-        sourceFile, err := os.Open(sourcePath)
+			err = Copy(sourceFile, destinationFile)
+			if err != nil {
+        
+				return err
+			}
+		}
+	}else{
+		sourceFile, err := os.Open(sourcePath)
 		if err != nil {
-		return err
+			return err
 		}
 		defer sourceFile.Close()
-
+	
 		destinationFile, err := os.Create(destinationPath)
 		if err != nil {
 			return err
 		}
 		defer destinationFile.Close()
-
+	
 		_, err = io.Copy(destinationFile, sourceFile)
 		if err != nil {
 			return err
 		}
-
+	
 		return nil
 	}
-}
+	return nil
+
+}		
+

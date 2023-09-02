a1b8d61ea92c89ba13c9ca23d92296becad81b6e
Null <auguselo77@gmail.com>
2023-09-02 18:13:48 -0300

    add dir

--- FileManager.go
+++ FileManager.go
@@ -3,24 +3,48 @@ package FileManager
 import (
 	"io"
 	"os"
+	"path/filepath"
 )
 func Copy(sourcePath, destinationPath string) error {
-	sourceFile, err := os.Open(sourcePath)
-	if err != nil {
-		return err
-	}
-	defer sourceFile.Close()
+	fileInfo, err := os.Stat(sourcePath)
 
-	destinationFile, err := os.Create(destinationPath)
-	if err != nil {
-		return err
-	}
-	defer destinationFile.Close()
+    if err != nil {
+        
+        return err
+    }
 
-	_, err = io.Copy(destinationFile, sourceFile)
-	if err != nil {
+    if fileInfo.IsDir() {
+		if err := os.MkdirAll(destinationPath, os.ModePerm); err != nil {
+			return err
+		}
+		entries, err := os.ReadDir(sourcePath)
+		if err != nil {
+			return err
+		}
+		for _, entry := range entries {
+			sourceFile := filepath.Join(sourcePath, entry.Name())
+			destinationFile := filepath.Join(destinationPath, entry.Name())
+			Copy(sourceFile, destinationFile)
+		}
+	
+    } else {
+        sourceFile, err := os.Open(sourcePath)
+		if err != nil {
 		return err
-	}
+		}
+		defer sourceFile.Close()
 
-	return nil
+		destinationFile, err := os.Create(destinationPath)
+		if err != nil {
+			return err
+		}
+		defer destinationFile.Close()
+
+		_, err = io.Copy(destinationFile, sourceFile)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
 }
